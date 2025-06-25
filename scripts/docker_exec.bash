af9ccc39920fdf89e7b77cbfc903f5ea08297246
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-06-24 21:34:48 -0400

    Update a Docker script.

--- scripts/docker_exec.bash
+++ scripts/docker_exec.bash
@@ -6,21 +6,23 @@ set -euo pipefail
 usage() {
   echo "Usage: $0 [options]
 
-Open a shell in the Docker container, optionally pulling or building the
-image first.
+Open a shell in the Docker container, optionally building the image first.
 
 Options:
-  --pull    Pull the public Docker image first.
   --build   Build the Docker image from scratch first.
   --cpu     Run in CPU-only mode.
 "
 }
 
 get_options=()
-start_options=(--gpus all --privileged)
+if [[ -f /etc/NIXOS ]]; then
+  start_options=(--device=nvidia.com/gpu=all)
+else
+  start_options=(--gpus all --privileged)
+fi
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --pull|--build) get_options+=("$1") ;;
+    --build) get_options+=("$1") ;;
     --cpu) start_options=() ;;
     --) shift; break ;;
     *) usage >&2; exit 1 ;;
@@ -33,6 +35,6 @@ dockerdev_ensure_dev_container_started "$DOCKER_DEV_IMAGE" \
   --x11 \
   -- \
   -v "$PWD":/app/ \
-  --mount type=bind,source="$HOME"/.ssh/id_rsa,destination=/home/"$USER"/.ssh/id_rsa \
+  --mount type=bind,source="$HOME"/.ssh/,destination=/home/"$USER"/.ssh/ \
   "${start_options[@]}"
 dockerdev_run_in_dev_container "$DOCKER_DEV_IMAGE" "$@"
